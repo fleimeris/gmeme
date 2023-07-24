6b635116c2a2d3083a355b9e83e360b50b7e6e39
fleimeris <44847763+fleimeris@users.noreply.github.com>
2023-07-24 22:53:03 +0300

    Add click handler for image

--- src/gmeme-gui.c
+++ src/gmeme-gui.c
@@ -7,49 +7,41 @@
 
 GuiMeme *memes;
 
-static void clicked(GtkButton* self)
+void released(GtkGestureClick* self, gint n_press, gdouble x, gdouble y, GuiMeme *meme)
 {
-    gmeme_app_data_insert_image("test", "test", "test");
+    g_print("%s\n", meme->image->title);
 }
 
 static void app_activate(GApplication *app)
 {
-    GtkWidget *win;
-    GtkWidget *box;
-    GtkWidget *btn;
+    GtkBuilder *builder = gtk_builder_new_from_resource("/org/github/fleimeris/gmeme/ui/gmeme-main.ui");
+
+    GtkWidget *win = GTK_WIDGET(gtk_builder_get_object(builder, "win"));
+    GtkWidget *box = GTK_WIDGET(gtk_builder_get_object(builder, "box"));
+
+    gtk_window_set_application(GTK_WINDOW(win), GTK_APPLICATION(app));
 
     GalleryImages *images = gmeme_app_data_get_all_images();
+    memes = (GuiMeme *) g_malloc(images->count * sizeof(GuiMeme));
 
-    win = gtk_application_window_new(GTK_APPLICATION(app));
-    gtk_window_set_title(GTK_WINDOW(win), "gMeme");
-    gtk_window_set_default_size(GTK_WINDOW(win), 600, 400);
-
-    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-    gtk_box_set_homogeneous(GTK_BOX(box), TRUE);
-
-    memes = g_malloc(images->count * sizeof(GuiMeme));
-
-/*    for(size_t i = 0; i < images->count; i++)
+    for(size_t i = 0; i < images->count; i++)
     {
         memes[i].image = &images->images[i];
         memes[i].gMemePaintable = gmeme_paintable_new_from_filepath(memes->image[i].absolutePath);
+
+        GtkGesture *gestureClick = gtk_gesture_click_new();
+        g_signal_connect(gestureClick, "released", G_CALLBACK(released), &memes[i]);
         memes[i].gImage = gtk_image_new_from_paintable(memes[i].gMemePaintable);
+        gtk_widget_set_receives_default(memes[i].gImage, TRUE);
+        gtk_widget_add_controller(memes[i].gImage, GTK_EVENT_CONTROLLER(gestureClick));
+
         gtk_box_append(GTK_BOX(box), memes[i].gImage);
-    }*/
-
-    gmeme_app_data_gallery_images_dispose_safe(images);
-
-    btn = gtk_button_new_with_label("Test insert");
-    g_signal_connect(btn, "clicked", G_CALLBACK(clicked), NULL);
-    gtk_box_append(GTK_BOX(box), btn);
+    }
 
     gtk_window_set_child(GTK_WINDOW(win), box);
-
     gtk_window_present(GTK_WINDOW(win));
 
-    GtkBuilder *builder = gtk_builder_new_from_resource("/org/github/fleimeris/gmeme/ui/gmeme-main.ui");
-    GtkWidget *window = GTK_WIDGET(gtk_builder_get_object(builder, "win"));
-    g_print("Window title: %s\n", gtk_window_get_title(GTK_WINDOW(window)));
+    g_object_unref(builder);
 }
 
 int gmeme_gui_run(int argc, char **argv)
@@ -61,6 +53,7 @@ int gmeme_gui_run(int argc, char **argv)
     g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
     stat = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
+    g_free(memes);
 
     return stat;
 }
